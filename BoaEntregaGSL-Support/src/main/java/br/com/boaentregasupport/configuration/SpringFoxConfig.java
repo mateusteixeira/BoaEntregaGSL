efa3780e40119b2651a8166c30205d89531407e2
mateusteixeira <mateusteixeira_@hotmail.com>
2022-04-17 21:11:38 -0300

    image

--- BoaEntregaGSL-Support/src/main/java/br/com/boaentregasupport/configuration/SpringFoxConfig.java
+++ BoaEntregaGSL-Support/src/main/java/br/com/boaentregasupport/configuration/SpringFoxConfig.java
@@ -1,11 +1,14 @@
 package br.com.boaentregasupport.configuration;
 
+import com.fasterxml.classmate.TypeResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -14,9 +17,8 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.time.Instant;
+import java.util.*;
 
 @Configuration
 @EnableSwagger2
@@ -35,8 +37,11 @@ public class SpringFoxConfig {
     }
 
     @Bean
-    public Docket api() {
+    public Docket api(TypeResolver typeResolver) {
         return new Docket(DocumentationType.SWAGGER_2)
+                .alternateTypeRules( AlternateTypeRules.newRule(
+                        typeResolver.resolve(Collection.class, Instant.class),
+                        typeResolver.resolve(Collection.class, Date.class), Ordered.HIGHEST_PRECEDENCE))
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.boaentregasupport.api"))
